a99c75ff85e83f5ae0ba76a98412bc9b7133238b
lalo.01.g@gmail.com <lalo.01.g@gmail.com>
2021-09-09 16:55:48 -0300

    created component

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,67 @@
-import React from 'react'
-
-import { ExampleComponent } from 'react-gold-modal'
-import 'react-gold-modal/dist/index.css'
+import React, { useEffect, useState } from 'react';
+import Modal from 'react-gold-modal'
+import 'react-gold-modal/src/styles.scss'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+	const [openModal, setOpenModal] = useState(false);
+	const [openModal2, setOpenModal2] = useState(false);
+	const [openModal3, setOpenModal3] = useState(false);
+
+	return <div>
+
+		<button onClick={() => setOpenModal(true)}>Open simple Modal</button>
+		<button onClick={() => setOpenModal2(true)}>Open large Modal</button>
+		<button onClick={() => setOpenModal3(true)}>Open vertical Modal</button>
+
+		<Modal
+			display={openModal}
+			title={'This is a modal'}
+			description={'Click OK to close'}
+			cancel={{
+				text: 'OK',
+				handler: () => setOpenModal(false),
+			}}
+		/>
+
+		<Modal
+			display={openModal2}
+			title={'This is a modal'}
+			description={'Click any option to close'}
+			body={<div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+			</div>}
+			options={[
+				{
+					text: 'Yes',
+					handler: () => setOpenModal2(false),
+				}
+			]}
+			cancel={{
+				text: 'No',
+				handler: () => setOpenModal2(false),
+			}}
+		/>
+
+		<Modal
+			isVertical
+			display={openModal3}
+			options={[
+				{
+					text: 'Save and Close',
+					handler: () => setOpenModal3(false),
+				},
+				{
+					text: 'Don´t Save and close',
+					handler: () => setOpenModal3(false)
+				}
+			]}
+			cancel={{
+				text: 'Just Close',
+				handler: () => setOpenModal3(false),
+			}}
+		/>
+	</div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,90 @@
-import React from 'react'
-import styles from './styles.module.css'
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable prettier/prettier */
+// import Spinner from '@/components/base/Spinner';
+import classNames from 'classnames';
+import React, { useEffect, useState } from 'react';
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const Modal = ({
+	display = false,
+	title = '',
+	description = '',
+	body = null,
+	options = [], // {text, handler, className}
+	className = '',
+	cancel = {
+		text: 'Cancel',
+		handler: () => false,
+	},
+	loading = false,
+	loadingDescription = 'Espere un instante...',
+	loadingSpinner = <p>Loading...</p>	,
+	isVertical = false,
+	...props
+}) => {
+	const [displayEffect, setDisplayEffect] = useState(false);
+	const [displayEffect2, setDisplayEffect2] = useState(false);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setDisplayEffect(display);
+		}, displayEffect ? 300 : 50);
+		setTimeout(() => {
+			setDisplayEffect2(display);
+		}, displayEffect ? 50 : 300);
+	}, [display]);
+
+
+	function InternalCancelHandler() {
+		cancel.handler && cancel.handler();
+	}
+
+	if (display || displayEffect) {
+		return (
+
+			<div className={classNames({ "--active": displayEffect2 }, "gold-modal")}>
+				<div className="modal__container">
+
+					<div className="modal__sub-container">
+						<div className="modal__card">
+							{title &&
+								<div className="card__title">
+									<h4 className="card__title-title">{title}</h4>
+									{description && body && <p className="card__title-subtitle">{description}</p>}
+								</div>
+							}
+
+							<div className={classNames('card__body', { 'd-none': loading || (!body && !description) })}>
+								{body ?
+									body
+									:
+									<p className="card__title-subtitle">{description}</p>
+								}
+							</div>
+
+							<div className={classNames('modal__options', { 'd-none': loading }, { '--vertical': isVertical })}>
+								{options && options.map((opt, i) => {
+									return <button key={i} onClick={opt.handler} className={classNames(opt.className, className)} >{opt.text}</button>
+								})}
+								<button key="cancel" onClick={InternalCancelHandler} className={classNames(cancel.className, className)} >{cancel.text}</button>
+							</div>
+
+							{loading &&
+								<>
+									{loadingSpinner}
+									{<p className="p-4 pt-0">{loadingDescription}</p>}
+								</>
+							}
+						</div>
+					</div>
+
+					<div className="modal__bg-close" onClick={InternalCancelHandler} />
+				</div>
+
+			</div>
+		)
+	} else {
+		return <React.Fragment />
+	}
 }
+
+export default Modal;
